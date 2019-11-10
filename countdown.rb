e524937d8eaf50ca49c01bcb0607db688ecd85d6
Richard Rosenthal <richard.rosenthal@acd.ccac.edu>
2019-11-10 00:58:05 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -9,3 +9,14 @@ def countdown(x)
 end
  "HAPPY NEW YEAR!"
 end
+
+def countdown_with_sleep(x)
+  def countdown(x)
+  x = 10 
+  while x > 0
+  puts "#{x} SECOND(S)!\n"
+  x -= 1
+  sleep(1)
+end
+ "HAPPY NEW YEAR!"
+end
