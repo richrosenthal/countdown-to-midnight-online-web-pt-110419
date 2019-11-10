a4e219ba890c70c7b5306824b453aa406d76cf0e
Richard Rosenthal <richard.rosenthal@acd.ccac.edu>
2019-11-10 00:59:21 +0000

    Done.

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,21 @@
 #write your code here
 
-def countdown
+def countdown(x)
+  x = 10 
+  while x > 0
+  puts "#{x} SECOND(S)!\n"
+  x -= 1
+  sleep(1)
+end
+ "HAPPY NEW YEAR!"
+end
+
+def countdown_with_sleep(x)
+  x = 10 
+  while x > 0
+  puts "#{x} SECOND(S)!\n"
+  x -= 1
+  sleep(1)
+end
+ "HAPPY NEW YEAR!"
 end
